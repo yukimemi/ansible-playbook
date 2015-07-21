250dac503f1f98082e23f7affa285e64114839cc
yukimemi <yukimemi@gmail.com>
2015-07-21 23:12:00 +0900

    Delete tmp dir before install.

--- i.sh
+++ i.sh
@@ -1,18 +1,22 @@
 #!bin/bash
 
+# (Before execute i.sh, need to do this commands.)
+# sudo xcodebuild -license
+# xcode-select --install
+
 set -e
 
+command rm -rf ~/.tmp
 mkdir ~/.tmp
 pushd ~/.tmp
 
 git clone https://github.com/yukimemi/mac-ansible.git
 pushd mac-ansible
 
-sudo xcodebuild -license
-xcode-select --install
-
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! type brew > /dev/null; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew install python
 brew install ansible
